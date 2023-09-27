23deac3695a1b78c1ee3f63e0af4e6aafaed5b8a
Alejandro Macazaga <alejandrolopezmacazaga@gmail.com>
2023-09-27 20:56:54 +0200

    Small change to VideoScene

--- Assets/Scripts/PlayVideo.cs
+++ Assets/Scripts/PlayVideo.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class PlayVideo : MonoBehaviour
 {
 
     [SerializeField] private int maxDistance = 100;
-
+    private VideoPlayer _video;
     private Camera _camera;
     // Start is called before the first frame update
     void Start()
     {
-        _camera = gameObject.GetComponent<Camera>();
+        _camera = Camera.main;
+        _video = gameObject.GetComponent<VideoPlayer>();
     }
 
     // Update is called once per frame
